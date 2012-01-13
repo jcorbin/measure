5453c15c43353b7e3bc49cefd0d9fda26477478a
Joshua T Corbin <jcorbin@wunjo.org>
2012-01-13 18:16:47 -0500

    Add program_result

--- program.h
+++ program.h
@@ -1,6 +1,9 @@
 #ifndef _PROGRAM_H
 #define _PROGRAM_H
 
+#include <sys/resource.h>
+#include <time.h>
+
 #include "error.h"
 
 struct program {
@@ -12,7 +15,22 @@ struct program {
     const char *stderr;
 };
 
+struct program_result {
+    const struct program *prog;
+    struct timespec start;
+    struct timespec end;
+    struct rusage rusage;
+    const char *stdin;
+    const char *stdout;
+    const char *stderr;
+};
+
 #define program_init() {NULL, NULL, NULL, NULL, NULL, NULL}
+
+#define program_result_init() {\
+    NULL, {0, 0}, {0, 0}, \
+    {{0, 0}, {0, 0}, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, \
+    NULL, NULL, NULL}
 
 int program_set_path(
     struct program *prog,
